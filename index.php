15ca422b3ecc4a257fa9682060543c11f3e60a9c
apocalipso <pruchkin-maksim@yandex.ru>
2022-03-25 08:56:47 +0300

    Внес правки

--- index.php
+++ index.php
@@ -2,16 +2,15 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Максим'; // укажите здесь ваше имя
-$category = array('Доски и лыжи','Крепления','Ботинки','Одежда','Инструменты','Разное');
-$infoAdvertisement = array(
-    array("2014 Rossignol District Snowboard","Доски и лыжи",10999,"img/lot-1.jpg"),
-    array("DC Ply Mens 2016/2017 Snowboard","Доски и лыжи",159999,"img/lot-2.jpg"),
-    array("Крепления Union Contact Pro 2015 года размер L/XL","Крепления",8000,"img/lot-3.jpg"),
-    array("Ботинки для сноуборда DC Mutiny Charocal","Ботинки",10999,"img/lot-4.jpg"),
-    array("Куртка для сноуборда DC Mutiny Charocal","Одежда",7500,"img/lot-5.jpg"),
-    array("Маска Oakley Canopy","Разное",5400,"img/lot-6.jpg
-")
-);
+$lots = ['Доски и лыжи','Крепления','Ботинки','Одежда','Инструменты','Разное'];
+$infoAdvertisement = [
+    ["nameproduct" => "2014 Rossignol District Snowboard","lot" => "Доски и лыжи","price" => 10999,"urlimg" => "img/lot-1.jpg"],
+    ["nameproduct" => "DC Ply Mens 2016/2017 Snowboard","lot" => "Доски и лыжи","price" => 159999,"urlimg" => "img/lot-2.jpg"],
+    ["nameproduct" => "Крепления Union Contact Pro 2015 года размер L/XL","lot" => "Крепления","price" => 8000,"urlimg" => "img/lot-3.jpg"],
+    ["nameproduct" => "Ботинки для сноуборда DC Mutiny Charocal","lot" => "Ботинки","price" => 10999,"urlimg" => "img/lot-4.jpg"],
+    ["nameproduct" => "Куртка для сноуборда DC Mutiny Charocal","lot" => "Одежда","price" => 7500,"urlimg" => "img/lot-5.jpg"],
+    ["nameproduct" => "Маска Oakley Canopy","lot" => "Разное","price" => 5400,"urlimg" => "img/lot-6.jpg"]
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -65,9 +64,9 @@ $infoAdvertisement = array(
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($category as $key => $value) :?>
+            <?php foreach ($lots as $lot) :?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=$value?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$lot?></a>
             </li>
             <?php endforeach;?>
         </ul>
@@ -78,18 +77,18 @@ $infoAdvertisement = array(
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <?php foreach ($infoAdvertisement as $key => $value) :?>
+            <?php foreach ($infoAdvertisement as $itemAdvertisement) :?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$infoAdvertisement[$key][3]?>" width="350" height="260" alt="">
+                    <img src="<?=$itemAdvertisement['urlimg']?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$infoAdvertisement[$key][1]?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$infoAdvertisement[$key][0]?></a></h3>
+                    <span class="lot__category"><?=$itemAdvertisement['lot']?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$itemAdvertisement['nameproduct']?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$infoAdvertisement[$key][2]?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$itemAdvertisement['price']?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -107,9 +106,9 @@ $infoAdvertisement = array(
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($category as $key => $value) :?>
+            <?php foreach ($lots as $lot) :?>
             <li class="nav__item">
-                <a href="pages/all-lots.html"><?=$value?></a>
+                <a href="pages/all-lots.html"><?=$lot?></a>
             </li>
             <?php endforeach;?>
         </ul>

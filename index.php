a836f7429fcd8830db6961e6e3e1259ae74ebc0c
Apocalipso <pruchkin-maksim@yandex.ru>
2022-03-30 13:55:01 +0300

    внес мелкие правки index.php

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
 require_once('helpers.php');
 require_once ('data.php');
-/**
- * Функция округляет число и добавляет знак рубля
- * @param int $price число
- * @return string цена с добавлением знака ₽
- */
 
 $page_content = include_template('main.php',['lots' => $lots,'categories' => $categories]);
 
 $layout_content = include_template('layout.php',[
     'is_auth'    => $is_auth,
-    'user_name'  => $user_name,
     'user_name'  => $user_name,
     'categories' => $categories,
     'content'    => $page_content,
@@ -20,14 +14,6 @@ $layout_content = include_template('layout.php',[
 
 print($layout_content);
 
-function formatNumber(int $price) :string
-{
-    if ($price<1000){
-        return ceil($price) . ' ₽';
-    }
-    $price = ceil($price);
-    return number_format($price, 0, null, ' ') . ' ₽';
-}
 ?>
 
 

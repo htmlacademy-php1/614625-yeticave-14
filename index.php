c8b8b0948a493609711c5c64a978c8c8f5c44390
Apocalipso <pruchkin-maksim@yandex.ru>
2022-04-07 13:28:25 +0300

    первая попытка сделать модуль5

--- index.php
+++ index.php
@@ -2,6 +2,27 @@
 require_once __DIR__ . '/init.php';
 require_once __DIR__ . '/data.php';
 
+if (!$link) {
+    $error = mysqli_connect_error();
+    $content = include_template('error.php',['error' => $error]);
+}
+else{
+    //запрос на показ категорий
+    $sql = 'SELECT `name`,`code` FROM categories';
+
+    $result = mysqli_query($link, $sql);
+
+    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    //получение лотов
+    $sql = 'SELECT lots.name,creation_time,img,begin_price,date_completion,categories.name as category FROM lots
+    LEFT JOIN categories on lots.category_id=categories.id';
+
+    $result = mysqli_query($link, $sql);
+
+    $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    
+}
 $page_content = include_template('main.php',['lots' => $lots,'categories' => $categories]);
 
 $layout_content = include_template('layout.php',[
--- init.php
+++ init.php
@@ -3,3 +3,7 @@ require_once __DIR__ . '/functions/calculate.php';
 require_once __DIR__ . '/functions/db.php';
 require_once __DIR__ . '/functions/template.php';
 require_once __DIR__ . '/functions/validate.php';
+
+$db = require_once  'databaseconnect.php';
+$link = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
+mysqli_set_charset($link, "utf8");

017edcb9eb040ca84452726e2ec09c24dc7ecf37
Apocalipso <pruchkin-maksim@yandex.ru>
2022-05-12 10:15:44 +0300

    перед написанием функционала поиска

--- search.php
+++ search.php
@@ -1,1 +1,21 @@
 <?php
+require_once __DIR__ . '/init.php';
+
+if (!$link) {
+    $error = mysqli_connect_error();
+    $content = include_template('error.php',['error' => $error]);
+}
+else{
+    $categories = getCategories($link);
+    $lots = getLots($link);
+}
+
+$page_content = include_template('search.php',['lots' => $lots,'categories' => $categories]);
+
+$layout_content = include_template('layout.php',[
+    'categories' => $categories,
+    'content'    => $page_content,
+    'title'      => 'Результаты поиска по запросу:' . $_GET['search'] 
+]);
+
+print($layout_content);

dac2e4dce48200d8d60c86b6f811c779e11ff207
unknown <Pruchkin-Maksim@yandex.ru>
2022-05-19 22:48:23 +0300

    в самом начале

--- init.php
+++ init.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/functions/db.php';
 require_once __DIR__ . '/functions/template.php';
 require_once __DIR__ . '/functions/validate.php';
 require_once __DIR__ . '/functions/upload.php';
+require_once __DIR__ . '/getwinner.php';
 
 $config = require 'config.php';
 
